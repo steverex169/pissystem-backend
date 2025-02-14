6b31a784c9bbcc4d0aa0165c3c1dd832a7ab64bb
labhazir <labhazir@gmail.com>
2025-02-14 15:06:18 +0500

    diffrent changings

--- databaseadmin/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from databaseadmin.models import City
-
-class CitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = City
-        fields = ('__all__')
-
