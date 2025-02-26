6241f1c31c49e1a91bb4a9af7e0ae31f37227082
Ali Ashraf <93863509+steverex169@users.noreply.github.com>
2025-02-26 19:16:49 +0500

    Update urls.py

--- organizationdata/urls.py
+++ organizationdata/urls.py
@@ -1,7 +1,6 @@
-# urls.py
 
 from django.urls import path
-from organizationdata.views import Statements2View, AccountsListsView, RegisterOrganizationView, OrganizationListView, OrganizationListUpdateAPIView, OrganizationListDeleteAPIView
+from organizationdata.views import NewsListView, NewsAddAPIView, PartnersList, Statements2View, AccountsListsView, RegisterOrganizationView, OrganizationListView, OrganizationListUpdateAPIView, OrganizationListDeleteAPIView
 
 urlpatterns = [
     path('register-organization', RegisterOrganizationView.as_view(), name='register-organization'),
@@ -10,7 +9,11 @@ urlpatterns = [
     path('delete-organization-list/<id>', OrganizationListDeleteAPIView.as_view(), name='delete-organization-list/<id>'),
     path('bank-account-statements1/<id>', Statements2View.as_view(), name='bank-account-statements1/<id>'),
     path('all-registered-partners/<id>', AccountsListsView.as_view(), name='all-registered-partners/<id>'),
-
+    path('partners-list', PartnersList.as_view(), name='partners-list'),
+    path('news-list/<id>',
+         NewsListView.as_view(), name='news-list'),
+    path('news-add',
+         NewsAddAPIView.as_view(), name='news-add'),
 ]
 
 
